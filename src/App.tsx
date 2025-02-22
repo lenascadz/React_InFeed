ee99912d8dcecb48c9e44d856ccf378692092055
Helena Scandian Trinta <hscandian@id.uff.br>
2025-02-22 18:02:01 -0300

    criando componentes de header-footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import Footer from "./components/footer";
+import Header from "./components/header";
+
 
 export default function App() {
   return (
-   <h1>hello</h1>
+    <>
+    <Header />
+
+    <Footer />
+    </>
+      
   )
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+      <nav>
+        <a href="/">Home</a> <br/>
+        <a href="/sobre">Sobre</a> <br/>
+        <a href="/contato">Contato</a>
+      </nav>
+    </header>
+  );
+}
